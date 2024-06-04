df31efcd5f201cc0b8cda7967c00bb69ad5ea59c
Youssef Achtatal <yachtata@c4r8p9.1337.ma>
2024-06-04 20:41:19 +0100

    push

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Define ANSI color codes
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [ "$1" == "--classter" ]; then
+    if [ "$2" == "mac" ]; then
+        classter="mac"
+    elif [ "$2" == "dell" ]; then
+        classter="dell"
+    else
+        echo "Invalid argument. Usage: $0 --classter [mac|dell]"
+        exit 1
+    fi
+else
+    echo "Usage: $0 --classter [mac|dell]"
+    exit 1
+fi
+
+if [ -z "$classter" ]; then
+    echo "Class argument is missing. Usage: $0 --classter [mac|dell]"
+    exit 1
+fi
+
+curl -fsSL https://raw.githubusercontent.com/regex-33/42-Move_Cache_goinfre/main/42-move_cache.sh > "$HOME/42-move_cache.sh"
+
+AUTOSTART_DIR="$HOME/.config/autostart/"
+mkdir -p "$AUTOSTART_DIR"
+
+echo "- Setting up Terminal to open automatically"
+cat > "$AUTOSTART_DIR/terminal.desktop" << EOF
+[Desktop Entry]
+Type=Application
+Exec=gnome-terminal --title="Move Cache" -- bash -c "\$HOME/42-move_cache.sh --classter $classter ; exec bash"
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=Terminal
+Name=Terminal
+Comment[en_US]=Start Terminal on login
+Comment=Start Terminal on login
+EOF
+
+echo -e "${GREEN}- Done! ✅${NC}"
+echo "- Now, log out and log back in."
+
---
+++ move_cache.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Define ANSI color codes
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+
+# Parse arguments
+if [ "$1" == "--classter" ]; then
+    if [ "$2" == "mac" ]; then
+        goinfre_dir="$HOME/goinfre/Apps"
+    elif [ "$2" == "dell" ]; then
+        goinfre_dir="$HOME/../../sgoinfre/goinfre/Perso/$USER/Apps"
+    else
+        echo "Invalid argument. Usage: $0 --classter [mac|dell]"
+        exit 1
+    fi
+else
+    echo "Usage: $0 --classter [mac|dell]"
+    exit 1
+fi
+
+# Define the directories to move
+declare -A dirs_to_move=(
+    ["Google"]="$HOME/.config/google-chrome"
+    ["JetBrains"]="$HOME/.config/JetBrains"
+    ["Mozilla"]="$HOME/.mozilla"
+    ["Postman"]="$HOME/.config/Postman"
+    ["Spotify"]="$HOME/.config/spotify"
+    ["iTerm2"]="$HOME/.config/iterm2"
+    ["Code"]="$HOME/.config/Code"
+    ["Docker Desktop"]="$HOME/.docker"
+    ["BraveSoftware"]="$HOME/.config/BraveSoftware"
+    ["Logitech"]="$HOME/.config/logitech"
+    ["org.videolan.vlc"]="$HOME/.config/vlc"
+    [".vscode"]="$HOME/.vscode"
+    [".var"]="$HOME/.var"
+    [".npm"]="$HOME/.npm"
+    [".local"]="$HOME/.local"
+)
+
+# Move directories
+for dir in "${!dirs_to_move[@]}"; do
+    original_dir="${dirs_to_move[$dir]}"
+    goinfre_dir_full="$goinfre_dir/$dir"
+
+    # Create the goinfre directory
+    mkdir -p "$goinfre_dir_full"
+    rm -rf "$original_dir"
+
+    # Create a symlink to the goinfre directory
+    ln -s "$goinfre_dir_full" "$original_dir"
+    echo -e "${GREEN}Moved $dir to goinfre :)${NC}"
+done
+
